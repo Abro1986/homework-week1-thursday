26df590d984d5c943fd62541dbb5ff14633eff42
Abro1986 <AndrewBroestl@gmail.com>
2018-03-22 17:47:11 -0600

    Commit 3 - Digit Sum.

--- soultions.js
+++ soultions.js
@@ -1,3 +1,9 @@
 // 2. a parameter is used in the parathensis of a function, an argument is passed into a function.
 // 3. console.log is just looged to the console and then deleted. a return stores a value
 
+function checkPalindrome(str) {
+	return str === str.toLowerCase().split("").reverse().join("");
+		console.log(str);
+}
+
+console.log(checkPalindrome("racecar"));
