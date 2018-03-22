58152e82a6e6b2141112ead68ed660ff860a8203
Abro1986 <AndrewBroestl@gmail.com>
2018-03-22 17:27:44 -0600

    Commit 1-Verbal questions

---
+++ soultions.js
@@ -0,0 +1,3 @@
+// 2. a parameter is used in the parathensis of a function, an argument is passed into a function.
+// 3. console.log is just looged to the console and then deleted. a return stores a value
+
