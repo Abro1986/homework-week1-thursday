8fe2cba8b6e4f55385b69329d0bf4f79c803729d
Abro1986 <AndrewBroestl@gmail.com>
2018-03-22 20:15:26 -0600

    Commit 3 - Digit Sum.

--- soultions.js
+++ soultions.js
@@ -2,8 +2,23 @@
 // 3. console.log is just looged to the console and then deleted. a return stores a value
 
 function checkPalindrome(str) {
-	return str === str.toLowerCase().split("").reverse().join("");
-		console.log(str);
+	return str.toLowerCase() === str.toLowerCase().split("").reverse().join("");
+		
 }
 
-console.log(checkPalindrome("racecar"));
+console.log(checkPalindrome("Racecar"));
+
+function sumDigits(num) {
+let newNum = num.toString().split(""); 
+let sum = 0
+console.log(newNum);
+	for (let i = 0; i <= newNum.length-1; i++) {
+		parseInt(newNum[i]);
+		sum = sum + parseInt(newNum[i]);
+	}; return sum
+
+}
+
+console.log(sumDigits(123))
+
+//
